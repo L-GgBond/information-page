268b8a1fc7b760c1b5fede16ce33a0e1fac8b82b
404 <948472619@qq.com>
2023-03-09 10:17:53 +0800

    Login

---
+++ src/views/Login.vue
@@ -0,0 +1,121 @@
+<template>
+    <div class="login_container">
+       <div class="login_box">
+           <div class="avatar_box">
+               <img src="../assets/logo.svg" alt="">
+            </div> 
+            <el-form ref="loginformref" label-width="0px" class="login_form">
+                <el-form-item>
+                     <el-input v-model="form.username" :prefix-icon="User" ></el-input>
+                </el-form-item> 
+
+                <el-form-item>
+                     <el-input v-model="form.password" :prefix-icon="Lock" type="password" show-password></el-input>
+                </el-form-item> 
+    
+                <el-form-item class="btns">
+                      <el-button type="primary" @click="doLogin">提交</el-button>
+                       <el-button type="info" @click="resetting">重置</el-button>
+                </el-form-item> 
+            </el-form>
+       </div>
+    </div>
+</template>
+
+<script setup>
+import { ref,reactive } from 'vue'
+import { User,Lock } from '@element-plus/icons-vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const form = reactive({
+    username: '',
+    password: '',
+})
+
+const doLogin = function () {
+    console.log("登陆")
+    console.log(form.username)
+    console.log(form.password)
+    router.push('/')
+}
+
+const resetting = function () {
+    console.log("重置")
+    form.username = ''
+    form.password = ''
+}
+
+const value = ref('')
+
+const options = [
+  {
+    value: 'Option1',
+    label: 'Option1',
+  },
+  {
+    value: 'Option2',
+    label: 'Option2',
+  },
+  {
+    value: 'Option3',
+    label: 'Option3',
+  },
+  {
+    value: 'Option4',
+    label: 'Option4',
+  },
+  {
+    value: 'Option5',
+    label: 'Option5',
+  },
+]
+</script>
+
+<style lang="less" scoped>
+.login_container{
+    background-color: #2b4b6b;
+    height:100%
+}
+.login_box{
+    width:450px;
+    height:300px;
+    background-color:#fff;
+    border-radius:3px;
+    position:absolute;
+    left:50%;
+    top:50%;
+    transform:translate(-50%,-50%);
+    .avatar_box{
+        height:130px;
+        width:130px;
+        border:1px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        box-shadow:0 0 10px #ddd;
+        position:absolute;
+        left:50%;
+        transform:translate(-50%,-50%);
+        background-color:#fff;
+       img {
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #eee;
+        }
+    }
+}
+
+.btns{
+    // display:flex;
+    // justify-content: flex-end;
+    float: right;
+}
+.login_form{
+    position: absolute;
+    bottom: 0;
+    width:100%;
+    padding:0 20px;
+    box-sizing: border-box;
+}
+
+</style>
